e299e995e1ea2bc793f80d7e2a2b26d545d17b7e
Babithavr <148205891+Babithavr@users.noreply.github.com>
2026-05-08 20:16:11 +0530

    Configure WebDriver with ChromeOptions for headless mode
    Added ChromeOptions for headless execution and modified WebDriver initialization.

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -3,27 +3,34 @@ package com.example;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
-public class App 
+public class App
 {
     public static void main(String[] args) throws InterruptedException
     {
-        WebDriver driver = new ChromeDriver();
-        Thread.sleep(2000);
+        ChromeOptions options = new ChromeOptions();
+
+        options.addArguments("--headless");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+
+        WebDriver driver = new ChromeDriver(options);
 
         driver.get("https://www.saucedemo.com/");
-        Thread.sleep(2000);
 
         driver.manage().window().maximize();
-        Thread.sleep(2000);
 
         driver.findElement(By.id("user-name")).sendKeys("standard_user");
-        Thread.sleep(2000);
 
         driver.findElement(By.id("password")).sendKeys("secret_sauce");
-        Thread.sleep(2000);
 
         driver.findElement(By.id("login-button")).click();
-        Thread.sleep(2000);
+
+        System.out.println("Login Successful");
+
+        Thread.sleep(3000);
+
+        driver.quit();
     }
 }
